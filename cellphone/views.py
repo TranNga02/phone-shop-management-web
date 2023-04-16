7a0b56bdad9fcf946699d40c1c896d67ff627fc0
TranNga02 <tranngadn02@gmail.com>
2023-04-17 01:26:11 +0700

    frontend

--- cellphone/views.py
+++ cellphone/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def sign_in(request):
+    return render(request, 'account/sign_in.html')
+
+def register(request):
+    return render(request, 'account/register.html')
+
+def cart(request):
+    return render(request, 'order/cart.html')
