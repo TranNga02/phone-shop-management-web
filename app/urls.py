61f911d64e0413560c4b98e48d2e532bd574bc30
chienkute <phamsichien112@gmail.com>
2023-05-18 15:26:29 +0700

    update final

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('',views.index,name="index"),
     path('', views.home, name='home'),
-    path('signin.html', views.sign_in, name='sign_in'),
-    path('register.html', views.register, name='register'),
-    path('cart.html', views.cart, name='cart'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('logout/', views.logoutPage, name='logout'),
+    path('register/', views.register, name='register'),
+    path('cart/', views.cart, name='cart'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('category.html', views.category, name='category'),
+    path('detail/<slug:id>/', views.detail, name='detail'),
+    path('place-order/',views.place_order,name='place-order'),
+    path('order_complete/',views.order_complete,name='order_complete'),
+    path('payment_success/',views.payment_success,name='payment_success'),
+    path('history/',views.history,name='history'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -78,15 +79,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
